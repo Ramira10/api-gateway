a10a0e6efdeb40d8ff31c61e49b6e7087400356f
Matías Ramira <matias.ramira@smartway.com.ar>
2023-05-29 12:03:02 -0300

    onfiguracion de RabbitMQ

---
+++ src/common/proxy/proxy.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { ClientProxySuperFlights } from "./client-proxy";
+
+@Module({
+    providers: [ClientProxySuperFlights],
+    exports: [ClientProxySuperFlights]
+})
+
+export class ProxyModule {
+    
+}
